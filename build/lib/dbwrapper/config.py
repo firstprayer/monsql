74d5bb2726b683254233de3b1e624113cdb0afcd
taiyuan.zhang <taiyuan.zhang@hulu.com>
2014-04-10 16:55:54 +0800

    batch commit after many update

---
+++ build/lib/dbwrapper/config.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+ASCENDING = 1
+DESCENDING = 2
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='dbwrapper',
+      version='0.1',
+      packages = find_packages(),
+      author='firstprayer',
+      email='zhangty10@gmail.com'
+      # package_dir={'dbwrapper': 'dbwrapper'},
+)
