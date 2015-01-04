3761bd63fe296ca5ce72b623cb282bb002c54e3b
Taiyuan Zhang <taiyuanz@Taiyuans-MacBook-Pro.local>
2015-01-03 20:37:01 -0500

    update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='monsql',
-      version='0.1.6',
+      version='0.1.7',
       packages = find_packages(),
       author='firstprayer',
       author_email='zhangty10@gmail.com',
